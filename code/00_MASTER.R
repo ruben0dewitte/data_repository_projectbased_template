29ba032cb95a105220aa16b2192fbc4ea9ac7d7b
Ruben Dewitte <ruben0dewitte@gmail.com>
2022-03-03 13:45:14 +0100

    initial commit

--- code/00_MASTER.R
+++ code/00_MASTER.R
@@ -16,11 +16,13 @@ source(here("code","01_setup.R"))
 
 # Dataprep - Prepare the data ----------------------------------------------------------------
 
-source(here("code","02_dataprep.R"))
+source(here("code","03_dataprep.R"))
 
-# Analysis - Analyse the data ----------------------------------------------------------------
+# Analysis ----------------------------------------------------------------
 
-source(here("code","03_analysis.R"))
+source(here("code","04_TraditionalGravityEstimates.R"))
+source(here("code","05_DistancePuzzle.R"))
+source(here("code","06_RTAs.R"))
 
 
 
